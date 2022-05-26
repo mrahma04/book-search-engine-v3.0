f37af55591aec68d4456bb38613637b33d729190
Mohammad Rahman <mbrahman@gmail.com>
2022-05-26 16:46:42 -0400

    added JWT when signing up users

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,28 @@
+const { User } = require('../models')
+const { AuthenticationError } = require('apollo-server-express')
+const { signToken } = require('../utils/auth')
+
 const resolvers = {
     Query: {
         helloWorld: () => {
             return 'Hello world!'
+        },
+        users: async () => {
+            return User.find()
+        },
+        user: async (parent, { _id }) => {
+            return User.findOne({ _id })
+            // .populate('posts')
+            // .populate('pets')
+        },
+    },
+
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args.input)
+            const token = signToken(user)
+
+            return { user, token }
         }
     }
 }
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,8 +1,35 @@
 const { gql } = require('apollo-server-express')
 
 const typeDefs = gql`
+
+    type User {
+        _id: ID
+        username: String
+        email: String
+        password: String
+    }
+
     type Query {
         helloWorld: String
+        users: [User]
+        user(_id: ID!): User
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    # create common input fields for mutations
+    input UserInput {
+        username: String
+        email: String
+        password: String
+    }
+
+
+    type Mutation {
+        addUser(input: UserInput!): Auth
     }
 `
 
